354762a14515a96acff5e7ecd06dfe0c14b5ba43
Stu Hoffman <stu@hoffmanswebsite.com>
2016-03-21 12:59:50 -0700

    Cleanup post names and number

--- AspNetBlog/src/AspNetBlog/Controllers/HomeController.cs
+++ AspNetBlog/src/AspNetBlog/Controllers/HomeController.cs
@@ -20,21 +20,21 @@ namespace AspNetBlog.Controllers
                 Title = "My Blog Post #1",
                 PostedDate = DateTime.Now,
                 Author = "Stu Hoffman",
-                Body = "This is the first blog post about ASP.NET MVC 6",
+                Body = "This is the first blog post about ASP.NET MVC 5",
                 },
                 new Post
                 {
                     Title = "My Blog Post #2",
                     PostedDate = DateTime.Now,
                     Author = "Stu Hoffman",
-                    Body = "This is the first blog post about ASP.NET MVC 6",
+                    Body = "This is the first blog post about ASP.NET MVC 5",
                 },
                 new Post
                 {
                     Title = "My Blog Post #3",
                     PostedDate = DateTime.Now,
                     Author = "Stu Hoffman",
-                    Body = "This is the first blog post about ASP.NET MVC 6",
+                    Body = "This is the first blog post about ASP.NET MVC 5",
                 }
 
             };
@@ -48,7 +48,7 @@ namespace AspNetBlog.Controllers
             post.Title = "My Blog Post";
             post.PostedDate = DateTime.Now;
             post.Author = "Stu Hoffman";
-            post.Body = "This is the first blog post about ASP.NET MVC 6";
+            post.Body = "This is the first blog post about ASP.NET MVC 5";
             return View(post);
         }
 
