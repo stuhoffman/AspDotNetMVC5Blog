fc53ad77234117859c02da2f94417a2b601474b0
Stu Hoffman <stu@hoffmanswebsite.com>
2016-03-21 11:23:11 -0700

    Fixed the first commit totally wrong folder

--- AspNetBlog/src/AspNetBlog/Controllers/HomeController.cs
+++ AspNetBlog/src/AspNetBlog/Controllers/HomeController.cs
@@ -11,55 +11,28 @@ namespace AspNetBlog.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly BlogDataContext _db;
-
-        public HomeController(BlogDataContext db)
-        {
-            _db = db;
-        }
-
         // GET: /<controller>/
-        public IActionResult Index(int page = 0)
-        {
-            var pageSize = 3;
-            var skip = page * pageSize;
-
-            var posts =
-                _db.Posts
-                    .OrderByDescending(x => x.PostedDate)
-                    .Skip(skip)
-                    .Take(pageSize)
-                    .ToArray();
-
-            var totalPosts = _db.Posts.Count();
-            var totalPages = totalPosts / pageSize;
-            var previousPage = page - 1;
-            var nextPage = page + 1;
-
-            ViewBag.PreviousPage = previousPage;
-            ViewBag.HasPreviousPage = previousPage >= 0;
-            ViewBag.NextPage = nextPage;
-            ViewBag.HasNextPage = nextPage < totalPages;
-
-            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
-                return PartialView(posts);
-
-            return View(posts);
-        }
-
-        public IActionResult Error()
+        public IActionResult Index()
         {
             return View();
         }
 
-        public int Echo(int id)
+        public IActionResult Post(long id)
+        {
+            var post = new Post();
+
+            post.Title = "My Blog Post";
+            post.PostedDate = DateTime.Now;
+            post.Author = "Stu Hoffman";
+            post.Body = "This is the first blog post about ASP.NET MVC 6";
+            return View(post);
+        }
+
+        public string Echo(string id)
         {
             return id;
         }
 
-        public IActionResult CauseAnError()
-        {
-            throw new Exception("Error!");
-        }
     }
+
 }
